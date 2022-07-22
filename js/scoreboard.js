967e392e0289a4b3de8bfda07cc38941d0b7dd3b
unknown <saritadh74@gmail.com>
2022-07-22 23:16:17 +0300

    added the functionality for the score message at the end relates #48

--- js/scoreboard.js
+++ js/scoreboard.js
@@ -1,26 +1,53 @@
 let users = JSON.parse(localStorage.getItem('users'));
-let container = document.querySelector('.scores');
+let finalScore = localStorage.getItem('finalScore');
 let main = document.querySelector('main');
+let container = document.querySelector('.scores');
+let message = document.querySelector('.message');
+let close = document.querySelector('.icon');
+let paragraphs = document.querySelectorAll('.message .content p');
 
-console.log(main);
+close.addEventListener('click', closeMessage);
 
-if (users) {
-  users = users.sort((a, b) => b.score - a.score);
-  users.forEach((e) => {
-    let userDiv = document.createElement('div');
-    container.appendChild(userDiv);
-
-    let name = document.createElement('span');
-    name.textContent = e.name;
-    userDiv.appendChild(name);
-
-    let score = document.createElement('span');
-    score.textContent = e.score;
-    userDiv.appendChild(score);
-  });
+if (finalScore !== null) {
+  container.style.display = 'none';
+  if (finalScore >= 5) {
+    paragraphs[0].textContent = `Your score is ${finalScore}/10 👍🏼`;
+    paragraphs[1].textContent = `Good Job`;
+  } else {
+    paragraphs[0].textContent = `Your score is ${finalScore}/10 👎🏼`;
+    paragraphs[1].textContent = `Try better`;
+  }
+  message.style.display = 'flex';
+  localStorage.removeItem('finalScore');
 } else {
-  let message = document.createElement('p');
-  message.textContent = 'No Players Yet';
-  container.textContent = '';
-  container.appendChild(message);
+  showScores();
+}
+
+function showScores() {
+  if (users) {
+    users = users.sort((a, b) => b.score - a.score);
+    users.forEach((e) => {
+      let userDiv = document.createElement('div');
+      container.appendChild(userDiv);
+
+      let name = document.createElement('span');
+      name.textContent = e.name;
+      userDiv.appendChild(name);
+
+      let score = document.createElement('span');
+      score.textContent = e.score;
+      userDiv.appendChild(score);
+    });
+  } else {
+    let message = document.createElement('p');
+    message.textContent = 'No Players Yet';
+    container.textContent = '';
+    container.appendChild(message);
+  }
+  container.style.display = 'block';
+}
+
+function closeMessage() {
+  message.style.display = 'none';
+  showScores();
 }
